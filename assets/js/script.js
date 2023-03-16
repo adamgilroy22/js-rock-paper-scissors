89fb50205040db8ee61efc97f24532e8af304650
Adam Gilroy <adamgilroy22@gmail.com>
2023-03-16 13:10:50 +0000

    Set up switch statement for win conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,27 @@ function getCompChoice() {
 
 }
 
+function game(userChoice){
+    const compChoice = getCompChoice();
+    switch (userChoice + compChoice) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            win();
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            lose();
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            draw();
+            break;
+    }
+}
+
 function main() {
     rock.addEventListener('click', function() {
         game("rock");
